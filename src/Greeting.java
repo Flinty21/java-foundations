6ac7d848e0f928137da7d18e76a43b6e519f37fd
Srikanth Paleti <163382814+Flinty21@users.noreply.github.com>
2026-06-30 18:46:49 +0530

    Testing out after updating the email address

---
+++ src/Greeting.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Greeting {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Write a message: ");
+        String message = scanner.nextLine();
+        System.out.println(message);
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main
 {
     public static void main(String[] args)
     {
-        System.out.println("This is my first print function.");
+        System.out.println("This is my first print function.\nChecking out?");
+        System.out.println("This is my second print function.");
     }
 }
